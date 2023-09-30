ac2945d67b396feb8696947e988fd5d0d9fadcd6
justaway41 <kritarthasapkota999@gmail.com>
2023-10-01 01:33:37 +0545

    added the onboarding screen layout

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:application/screens/login_view.dart';
+import 'package:application/screens/onboarding_view.dart';
+import 'package:application/screens/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+  runApp(const Onboarding(
+    title: "Title 1",
+    description: "description 1",
+  ));
 }
