359cccfd57f26357b60c906fdc2c81a380feaab7
Prasansha Bharati <prasanshabharati@gmail.com>
2023-09-30 23:00:45 +0545

    completed authentication middleware with oauth

---
+++ api/index.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const { auth } = require("express-oauth2-jwt-bearer");
+const { addNewUser } = require("./middlewares/addNewUser");
+
+require("dotenv").config({
+    path:"./.env"
+})
+global.appRoot = __dirname;
+
+mongoose
+.connect("mongodb://127.0.0.1:27017/KUHackFest")
+.then(() => console.log("Database connected..."))
+.catch((err) => console.log(err.message));
+    
+const app = express();
+
+const newUser = (req, res, next) => {
+    console.log(req.auth)
+    next();
+}
+
+const validateAccessToken = auth({
+    issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+    audience: process.env.AUTH0_AUDIENCE,
+});
+
+
+// Public API endpoints
+
+// Protected API endpoints
+app.use(validateAccessToken, addNewUser);
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running at port ${process.env.PORT}...`);
+});
